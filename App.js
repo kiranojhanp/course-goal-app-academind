303345642bea2eed507e83e116fcc33db032764c
Kiran <kiranojha1226@gmail.com>
2021-03-05 16:41:36 +0545

    basic styling

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+  Button,
+} from "react-native";
 
 export default function App() {
+  const [outputText, setOutputText] = useState("Hello, World");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          flex: 1,
+        }}
+      >
+        <TextInput placeholder="Please enter a name" style={styles.textInput} />
+        <Button title="ADD" />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          flex: 1,
+        }}
+      >
+        <TextInput placeholder="Please enter a name" style={styles.textInput} />
+        <Button title="ADD" />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          flex: 1,
+        }}
+      >
+        <TextInput placeholder="Please enter a name" style={styles.textInput} />
+        <Button title="ADD" />
+      </View>
+      <StatusBar style="dark" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
+    flexDirection: "column",
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  textInput: {
+    height: 30,
+    width: "50%",
+    borderBottomColor: "black",
+    borderBottomWidth: 1,
   },
 });
